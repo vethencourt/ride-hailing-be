ef42ea85e5bbda3d154c85a987e14dac7162c08e
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-27 00:59:01 -0400

    chore: connect to local mongodb

---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env['MONGODB_URI'] as string)
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error: ${(error as Error).message}`)
+    process.exit(1)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import router from './router/index.js'
+import { connectDB } from './config/db.js' // Import connection
 
 dotenv.config()
 
 const app = express()
-const PORT = 8000
+const PORT = process.env['PORT'] || 8000
 
+connectDB()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
