a8bb28c30a036e0ec39eddbf82782b0bac096113
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-27 12:17:31 -0400

    refactor: rename functions to be more descriptive

--- src/controllers/meta.controller.ts
+++ src/controllers/meta.controller.ts
@@ -1,17 +1,17 @@
 import type { Request, Response } from 'express'
 
-export const getMakes = async (request: Request, response: Response) => {
+export async function getMakes(request: Request, response: Response) {
   console.log(request)
   response.send('Car makes')
 }
 
-export const getModels = async (request: Request, response: Response) => {
+export async function getModels(request: Request, response: Response) {
   const { makeId } = request.params
   console.log(makeId)
   response.send('Car models')
 }
 
-export const getYears = async (request: Request, response: Response) => {
+export async function getYears(request: Request, response: Response) {
   const { modelId } = request.params
   console.log(modelId)
   response.send('Car years')
--- src/controllers/vehicles.controller.ts
+++ src/controllers/vehicles.controller.ts
@@ -1,28 +1,28 @@
 import type { Request, Response } from 'express'
 
-export const getVehicles = async (request: Request, response: Response) => {
+export async function getVehicles(request: Request, response: Response) {
   console.log(request)
   response.send('Vehicles list')
 }
 
-export const getVehicle = async (request: Request, response: Response) => {
+export async function getVehicle(request: Request, response: Response) {
   const { id } = request.params
   console.log(id)
   response.send('Vehicle details')
 }
 
-export const createVehicle = async (request: Request, response: Response) => {
+export async function createVehicle(request: Request, response: Response) {
   console.log(request)
   response.send('Vehicle creation')
 }
 
-export const updateVehicle = async (request: Request, response: Response) => {
+export async function updateVehicle(request: Request, response: Response) {
   const { id } = request.params
   console.log(id)
   response.send('Vehicle update')
 }
 
-export const deleteVehicle = async (request: Request, response: Response) => {
+export async function deleteVehicle(request: Request, response: Response) {
   const { id } = request.params
   console.log(id)
   response.send('Vehicle deletion')
