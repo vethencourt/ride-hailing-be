b538b18239771c272ed867eb84419605ad787506
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-27 14:56:10 -0400

    fet: implement auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,53 @@
+import { User } from '../models/user.model.js'
+import type { IRegisterUser, ILoginCredentials, ILoginResponse } from '../types/auth.types.js'
+import type { ServiceResponse } from '../types/types.js'
+import { hashPassword, verifyPassword } from '../utils/crypto.js'
+import { generateToken } from '../utils/jwt.js'
+
+export const signup = async (data: IRegisterUser): Promise<ServiceResponse<{}>> => {
+  try {
+    const user = new User({
+      email: data.email.toLowerCase(),
+      password: await hashPassword(data.password)
+    })
+
+    await user.save()
+    return { success: true, data: {} }
+  } catch (error: any) {
+    if (error.code === 11000)
+      return {
+        success: false,
+        error: 'Email already exists',
+        code: 400
+      }
+
+    return {
+      success: false,
+      error: 'Failed to create account',
+      code: 500
+    }
+  }
+}
+
+export const login = async (
+  credentials: ILoginCredentials
+): Promise<ServiceResponse<ILoginResponse>> => {
+  const noAuthError: ServiceResponse<ILoginResponse> = {
+    success: false,
+    error: 'Credenciales inválidas',
+    code: 401
+  }
+  const userDoc = await User.findOne({ email: credentials.email.toLowerCase() })
+  if (!userDoc) return noAuthError
+
+  const { id, email, password } = userDoc
+  const isMatch = await verifyPassword(credentials.password, userDoc.password)
+  if (!isMatch) return noAuthError
+
+  const token = generateToken({ id, email })
+
+  if (!token) return { success: false, error: 'No se pudo generar el token', code: 500 }
+  const user = { id, email, password }
+
+  return { success: true, data: { user, token } }
+}
