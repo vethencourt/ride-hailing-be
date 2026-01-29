723c96a278928cb385daed8c7d642882a84774d4
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-28 22:14:46 -0400

    fix: use correct validation on vehicle update

--- src/router/vehicles.router.ts
+++ src/router/vehicles.router.ts
@@ -6,14 +6,14 @@ import {
   getVehicles,
   updateVehicle
 } from '../controllers/vehicles.controller.js'
-import { validateBody, vehicleCreateSchema } from '../utils/validation.js'
+import { validateBody, vehicleCreateSchema, vehicleUpdateSchema } from '../utils/validation.js'
 
 const router: Router = Router()
 
 router.post('/', getVehicles)
 router.get('/:id', getVehicle)
 router.post('/create', validateBody(vehicleCreateSchema), createVehicle)
-router.put('/:id/', validateBody(vehicleCreateSchema), updateVehicle)
+router.put('/:id/', validateBody(vehicleUpdateSchema), updateVehicle)
 router.delete('/:id', deleteVehicle)
 
 export default router
