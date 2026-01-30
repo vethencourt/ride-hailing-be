900ea85f7ba359c23217626d35aea7740f2b54e5
Ricardo Vethencourt <ricardo.vethencourt.m@gmail.com>
2026-01-29 22:46:51 -0400

    fix: add health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import router from './router/index.js'
-import { connectDB } from './config/db.js' // Import connection
+import { connectDB } from './config/db.js'
 
 dotenv.config()
 
@@ -12,6 +12,7 @@ const PORT = process.env['PORT'] || 8000
 connectDB()
 app.use(cors())
 app.use(express.json())
+app.get('/', (_, response) => response.send('API is running...')) // health check
 app.use('/api', router)
 
 app.listen(PORT, () => console.log(`live at ${PORT}`))
